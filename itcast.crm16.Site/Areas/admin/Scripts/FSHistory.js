76e703ecf654d5f95871043e17a74c3f9c8fbf53
jinqiang.zhu <jinqiang.zhu@chukou1.com>
2016-12-15 18:31:06 +0800

    Merge remote-tracking branch 'origin/Jay/member'
    Conflicts:
    	itcast.crm16.IRepository/itcast.crm16.IRepository.csproj
    	itcast.crm16.IServices/itcast.crm16.IServices.csproj
    	itcast.crm16.Repository/Repository.tt
    	itcast.crm16.Site/itcast.crm16.Site.csproj
    	itcast.crm16.WebHelper/BaseController.cs
    	itcast.crm16.model/FS_Date.Context.cs
    	itcast.crm16.model/FS_Date.Designer.cs
    	itcast.crm16.model/FS_Date.edmx
    	itcast.crm16.model/FS_Date.edmx.diagram
    	itcast.crm16.model/FS_Date.tt
    	itcast.crm16.model/itcast.crm16.model.csproj

---
+++ itcast.crm16.Site/Areas/admin/Scripts/FSHistory.js
@@ -0,0 +1,14 @@
+﻿(function () {
+    var $modal = $('#doc-modal-1');
+
+    $('#commerceAdd').on('click', function (e) {
+
+        SetNewModel(0, "", "");
+        //使用详情看 http://amazeui.org/javascript/modal
+
+        $modal.modal({
+            width: 800,
+            height: 650
+        });
+    });
+})
