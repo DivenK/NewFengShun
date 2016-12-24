e7a9444339d827a1181ac301f3ba09201c3352e6
Jay <935812471@qq.com>
2016-12-24 17:29:40 +0800

    会员数据收集

---
+++ itcast.crm16.Site/Controllers/memberController.cs
@@ -0,0 +1,46 @@
+﻿using itcast.crm16.WebHelper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using itcast.crm16.IServices;
+using itcast.crm16.model;
+
+namespace itcast.crm16.Site.Controllers
+{
+
+    [WebHelper.Attrs.SkipCheckLogin]
+    public class memberController : BaseController
+    {
+        public memberController(IsysMenusServices mSer, IMemberMsgServices memberMsgSer) : base(mSer)
+        {
+            base.memberMsgSer = memberMsgSer;
+        }
+
+        //
+        // GET: /member/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AddMember(MemberMsg entity)
+        {
+            entity.CreateDate = DateTime.Now;
+            entity.IsDelete = false;
+            memberMsgSer.Add(entity);
+            int addRet = memberMsgSer.SaveChanges();
+            if(addRet>0)
+            {
+                return WriteSuccess("");
+            }
+            else
+            {
+                return WriteError("");
+            }
+        }
+
+    }
+}
