c7e221123cf409df6909ad554c33e546cbfd6fde
DevinK <631916104@qq.com>
2016-12-25 14:39:57 +0800

    update

--- itcast.crm16.Site/Controllers/CemmendController.cs
+++ itcast.crm16.Site/Controllers/CemmendController.cs
@@ -21,6 +21,7 @@ namespace itcast.crm16.Site.Controllers
         {
             var list = Commerce.GetItemModel(1, "", out TotalPage, 0, 10, true).ToList();
             ViewBag.items= list.OrderByDescending(c => c.Nid).ToList();
+            ViewBag.id = 0;
             return View();
         }
         public ActionResult GetModel(int id=0,int type=0)
@@ -31,12 +32,48 @@ namespace itcast.crm16.Site.Controllers
             return View();
         }
 
-        public ActionResult GetLeader()
+        public ActionResult GetLeader(int id=0)
         {
-            var list = Commerce.GetItemModel(1, "", out TotalPage,1, 10, true).ToList();
+            var list = Commerce.GetItemModel(1, "", out TotalPage,0, 10, true).ToList();
             ViewBag.items = list.OrderByDescending(c => c.Nid).ToList();
-          
+            var lists = Commerce.GetItemModel(1, "", out TotalPage,2,3, true).ToList();
+            base.pageSize = 3;
+            SetViewBagPage();
+            ViewBag.list = list;
+            ViewBag.Items = lists;
+            ViewBag.id = id;
             return View();
         }
+        public ActionResult AjaxGetLeader(int pageIndex)
+        {
+            var lists = Commerce.GetItemModel(pageIndex, "", out TotalPage, 2,3, true).ToList();
+           
+            if (lists.Count > 0)
+            {
+                return WriteSuccess("", lists);
+            }
+            else
+            {
+                return WriteError("");
+            }
+        }
+
+        public ActionResult LeaderModel(int id)
+        {
+            var list = Commerce.GetItemModel(1, "", out TotalPage, 0, 10, true).OrderByDescending(c => c.Nid).ToList();
+            ViewBag.list = list;
+            if (id > 0)
+            {
+            
+                var model = Commerce.QueryWhere(c => c.id == id).First();
+                ViewBag.Model = model;
+            }
+            else
+            {
+                ViewBag.Model = null;
+            }
+            return View();
+        }
+
     }
 }
